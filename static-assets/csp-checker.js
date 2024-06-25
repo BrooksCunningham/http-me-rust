4f9a266e303756d885662269a750912d04127e03
Brooks Cunningham <bcunningham@fastly.com>
2024-06-25 15:30:06 -0500

    Update csp-checker.js

--- static-assets/csp-checker.js
+++ static-assets/csp-checker.js
@@ -1,13 +1,5 @@
-document.addEventListener("securitypolicyviolation", function (e) {
-
-    alert("security policy violation!");
-})
-
 addEventListener("securitypolicyviolation", (event) => {
     console.log(event);
-});
-
-addEventListener("securitypolicyviolation", (event) => {
     const violationData = {
         'document-uri': event.documentURI,
         'referrer': event.referrer,
