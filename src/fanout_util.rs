857b3a2a62f122993f1b5c4b8ab5854b744fe6c6
Brooks Cunningham <bcunningham@fastly.com>
2024-08-06 12:27:03 -0500

    Commits

---
+++ src/fanout_util.rs
@@ -0,0 +1,61 @@
+use fastly::http::StatusCode;
+use fastly::Response;
+use fastly::Request;
+
+/// Returns a GRIP response to initialize a stream
+///
+/// When Compute receives a non-WebSocket request (i.e. normal HTTP) and wants
+/// to make it long lived (longpoll or SSE), we call handoff_fanout on it, and
+/// Fanout will then forward that request to the nominated backend.  In this app,
+/// that backend is this same Compute service, where we then need to respond
+/// with some Grip headers to tell Fanout to hold the connection for streaming.
+/// This function constructs such a response.
+pub fn grip_response(ctype: &str, ghold: &str, chan: &str) -> Response {
+    Response::from_status(StatusCode::OK)
+        .with_header("Content-Type", ctype)
+        .with_header("Grip-Hold", ghold)
+        .with_header("Grip-Channel", chan)
+        .with_body("")
+}
+
+/// Returns a WebSocket-over-HTTP formatted TEXT message
+pub fn ws_text(msg: &str) -> Vec<u8> {
+    format!("TEXT {:02x}\r\n{}\r\n", msg.len(), msg)
+        .as_bytes()
+        .to_vec()
+}
+
+// Returns a channel-subscription command in a WebSocket-over-HTTP format
+pub fn ws_sub(ch: &str) -> Vec<u8> {
+    ws_text(format!("c:{{\"type\":\"subscribe\",\"channel\":\"{}\"}}", ch).as_str())
+}
+
+pub fn handle_fanout_ws(mut req: Request, chan: &str) -> Response {
+    if req.get_header_str("Content-Type") != Some("application/websocket-events") {
+        return Response::from_status(StatusCode::BAD_REQUEST)
+            .with_body("Not a WebSocket-over-HTTP request.\n");
+    }
+
+    let req_body: Vec<u8> = req.take_body().into_bytes();
+    println!("req_body for ws");
+    println!("{:?}", std::str::from_utf8(&req_body));
+    let mut resp_body: Vec<u8> = [].to_vec();
+
+    let mut resp = Response::from_status(StatusCode::OK)
+        .with_header("Content-Type", "application/websocket-events");
+
+    if req_body.starts_with(b"OPEN\r\n") {
+        resp.set_header("Sec-WebSocket-Extensions", "grip; message-prefix=\"\"");
+        resp_body.extend("OPEN\r\n".as_bytes());
+        resp_body.extend(ws_sub(chan));
+    } else if req_body.starts_with(b"TEXT ") {
+        resp_body.extend(ws_text(
+            format!("You said: {}", std::str::from_utf8(&req_body).unwrap_or("")).as_str(),
+        ));
+    }
+    println!("ws resp_body");
+    println!("{:?}", std::str::from_utf8(&resp_body));
+
+    resp.set_body(resp_body);
+    return resp
+}
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use std::thread::sleep;
 use std::io::Write;
 use std::time::Duration;
 
+mod fanout_util;
+
 fn main() -> Result<(), Error> {
     let client_req = Request::from_client();
     let mut server_resp = handler(client_req)?;
@@ -61,6 +63,7 @@ fn handler(mut req: Request) -> Result<Response, Error> {
         s if s.starts_with("/anything") => return Ok(anything(req, resp)?),
         s if s.starts_with("/static-assets/") => return Ok(get_static_asset(&req, resp)?),
         s if s.starts_with("/forms/post") => return Ok(get_static_asset(&req, resp)?),
+        s if s.starts_with("/chatroom") => return Ok(chatroom(req, resp)?),
 
         "/" => return Ok(swagger_ui_html(resp)?),
         
@@ -105,13 +108,13 @@ fn anything(mut req: Request, mut resp: Response) -> Result<Response, Error> {
 
 fn status(mut req: &Request, mut resp: Response) -> Result<Response, Error> {
     let mut status_str = "";
-    let mut statusParsed = 200;
+    let mut status_parsed = 200;
 
     match req.get_header_str("endpoint") {
         Some(ep) if ep.contains("status") => {
             status_str = ep.split("=").collect::<Vec<&str>>()[1];
-            statusParsed = status_str.parse::<u16>()?;
-            return status_result(statusParsed, resp);
+            status_parsed = status_str.parse::<u16>()?;
+            return status_result(status_parsed, resp);
         },
         _ => ()
     }
@@ -128,9 +131,9 @@ fn status(mut req: &Request, mut resp: Response) -> Result<Response, Error> {
     }
 
     status_str = path_segments[1];
-    statusParsed = status_str.parse::<u16>()?;
+    status_parsed = status_str.parse::<u16>()?;
 
-    return status_result(statusParsed, resp);
+    return status_result(status_parsed, resp);
 
     fn status_result(status_u16: u16, mut resp: Response) -> Result<Response, Error> {
         return match status_u16 {
@@ -191,6 +194,23 @@ fn get_static_asset(req: &Request, mut resp: Response) -> Result<Response, Error
     return Ok(resp)
 }
 
+
+fn chatroom(mut req: Request, mut resp: Response) -> Result<Response, Error> {
+    // resp.set_body_text_plain("chatroom response");
+    let chan = "chatroomtest";
+    let resp = match req.get_url().path() {
+        // "/chatroom" => fanout_util::handle_fanout_ws(req, chan),
+        "/chatroom" => fanout_util::grip_response("text/plain", "response", chan),
+        "/test/long-poll" => fanout_util::grip_response("text/plain", "response", chan),
+        "/test/long-poll" => fanout_util::grip_response("text/plain", "response", chan),
+        "/test/stream" => fanout_util::grip_response("text/plain", "stream", chan),
+        "/test/sse" => fanout_util::grip_response("text/event-stream", "stream", chan),
+        "/test/websocket" => fanout_util::handle_fanout_ws(req, chan),
+        _ => Response::from_status(StatusCode::NOT_FOUND).with_body("No such test endpoint\n"),
+    };
+    println!("{:?}",resp);
+    return Ok(resp)
+}
 
 #[test]
 fn test_homepage() {
